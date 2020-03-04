7b1bebd8b3110fff582d50643c3b27b08d6a40f8
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2020-03-04 22:36:23 +0100

    document ready

--- mapbox/js/js.js
+++ mapbox/js/js.js
@@ -2,63 +2,68 @@
  * file: js/js.js
  * project: mapbox workshop suggested solution
  **/
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhdGhvb3IiLCJhIjoiY2oyd3hlbzU3MDA5NzJxbm9iMjczanJndCJ9.HahDB7Z1rrD5THIYQh6t4g';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/asathoor/ck7d5gplq11od1imzpb8hobsz',
-    center: [-74.5, 40],
-    zoom: 17,
-});
+$(document).ready(function () {
+    console.log("hi from js/js.js - the document is ready.");
 
-map.on('load', function () {
-    map.loadImage(
-        'images/atache.png',
-        function (error, image) {
-            if (error) throw error;
-            map.addImage('cat', image);
-            map.addSource('point', {
-                'type': 'geojson',
-                'data': {
-                    'type': 'FeatureCollection',
-                    'features': [
-                        {
-                            'type': 'Feature',
-                            'geometry': {
-                                'type': 'Point',
-                                'coordinates': [10, 56]
-                            }
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhdGhvb3IiLCJhIjoiY2oyd3hlbzU3MDA5NzJxbm9iMjczanJndCJ9.HahDB7Z1rrD5THIYQh6t4g';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/asathoor/ck7d5gplq11od1imzpb8hobsz',
+        center: [-74.5, 40],
+        zoom: 17,
+    });
+
+    map.on('load', function () {
+        map.loadImage(
+            'images/atache.png',
+            function (error, image) {
+                if (error) throw error;
+                map.addImage('cat', image);
+                map.addSource('point', {
+                    'type': 'geojson',
+                    'data': {
+                        'type': 'FeatureCollection',
+                        'features': [
+                            {
+                                'type': 'Feature',
+                                'geometry': {
+                                    'type': 'Point',
+                                    'coordinates': [10, 56]
+                                }
                             }
                         ]
-                }
-            });
-            map.addLayer({
-                'id': 'points',
-                'type': 'symbol',
-                'source': 'point',
-                'layout': {
-                    'icon-image': 'cat',
-                    'icon-size': 0.25
-                }
-            });
-        }
-    );
-});
-
-/* jQuery Stuff */
-$('#btn_1').click(function () {
-    
-    // using flyTo options
-    map.flyTo({
-        center: [10, 56],
-        zoom: 9,
-        speed: .8,
-        curve: 2,
-        easing(t) {
-            return t;
-        }
+                    }
+                });
+                map.addLayer({
+                    'id': 'points',
+                    'type': 'symbol',
+                    'source': 'point',
+                    'layout': {
+                        'icon-image': 'cat',
+                        'icon-size': 0.25
+                    }
+                });
+            }
+        );
     });
-    
-    // chage style after a click
-    $(this).css("background-color", "lime");
-    $(this).css("color", "black");
-})
+
+    /* jQuery Stuff */
+    $('#btn_1').click(function () {
+
+        // using flyTo options
+        map.flyTo({
+            center: [10, 56],
+            zoom: 9,
+            speed: .8,
+            curve: 2,
+            easing(t) {
+                return t;
+            }
+        });
+
+        // chage style after the click
+        $(this).css("background-color", "lime"); // this === #btn_1
+        $(this).css("color", "black");
+    })
+
+}); // ends the document ready function
